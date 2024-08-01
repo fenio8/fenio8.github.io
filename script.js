137566a3557b39234a04a6d8013dc23670249b8e
fenio8 <fenho.fv@gmail.com>
2024-08-01 05:21:19 -0400

    image3

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const audio = document.getElementById('audio');
     const body = document.getElementById('body');
+    const imageContainer = document.querySelector('.image-container img');
+    const images = [
+        './img/fotito2.png',
+        './img/fotito4.png',
+        './img/fotito5.png',
+        './img/fotito3.png'
+    ];
+    let currentImageIndex = 0;
 
+    // Configurar el volumen inicial
+    audio.volume = 0.1;
 
-    audio.volume = 0.2;
-
-
+    // Función para cambiar el fondo según el estado del audio
     function updateBackground() {
         if (audio.paused) {
             body.classList.remove('playing');
@@ -16,10 +24,19 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-
+    // Actualizar el fondo cuando el audio empieza o se pausa
     audio.addEventListener('play', updateBackground);
     audio.addEventListener('pause', updateBackground);
 
-
+    // Configuración inicial
     updateBackground();
+
+    // Función para cambiar la imagen
+    function changeImage() {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        imageContainer.src = images[currentImageIndex];
+    }
+
+    // Cambiar la imagen cada 5 segundos
+    setInterval(changeImage, 5000);
 });
